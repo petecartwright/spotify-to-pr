aa4a552a6b37eaeb109aef90879e53bd8968103e
Pete Cartwright <pete@petecartwright.com>
2024-12-07 09:53:28 -0500

    Update main.ts

--- src/entries/popup/main.ts
+++ src/entries/popup/main.ts
@@ -4,48 +4,74 @@ import { MESSAGE_ACTIONS, STORAGE_KEYS } from "~/util/keys";
 
 const imageUrl = new URL(logo, import.meta.url).href;
 
-const loginUnexpired = (await chrome.storage.local.get(STORAGE_KEYS.expiresAt))
-  .expiresAt;
+const urlIsGithub = async () => {
+  // if we're not on github.com AND don't see an open pr comment, get upset
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  // TODO: this could be better
+  if (!tab.url) throw new Error("not in a tab????");
+  const currentUrl = new URL(tab.url);
 
-let loginButton = "";
-if (!loginUnexpired) {
-  loginButton = `<button type="button" name="loginButton">
+  return currentUrl.hostname !== "github.com";
+};
+
+const main = async () => {
+  const isGithub = await urlIsGithub();
+  if (!isGithub) return;
+
+  const loginExpiration = (
+    await chrome.storage.local.get(STORAGE_KEYS.expiresAt)
+  ).expiresAt;
+
+  const loginUnexpired = Date.now() < loginExpiration;
+
+  let loginButton = "";
+  if (!loginUnexpired) {
+    loginButton = `<button type="button" name="loginButton">
   Login
   </button>`;
-}
+  }
 
-// TODO: maybe we show nothing if we're not on github
-//       or we are on github and don't have an open PR editor open
+  // TODO: only show on approp github pages????
+  //       and need to check
 
-document.querySelector("#app")!.innerHTML = `
+  // TODO: get rid of the popup altogether
+  //    if we click the button, we try to hit the API
+  //    if it works, we just do it
+  //    if we're unauth'd, direct to auth flow
+
+  // TODO: maybe we show nothing if we're not on github
+  //       or we are on github and don't have an open PR editor open
+
+  document.querySelector("#app")!.innerHTML = `
   <img src="${imageUrl}" height="45" alt="" />
-  <h1>Login</h1>
 	${loginButton}
 	${
     loginButton
       ? ""
-      : `<button type="button" name="writeToPrButton">
-          Write To PR
+      : `<h1>Login</h1>
+            <button type="button" name="writeToPrButton">
+            Write To PR
         </button>`
   }
 `;
 
-if (loginButton) {
-  const loginButtonElement = document.querySelector(
-    "button[name='loginButton']"
-  ) as HTMLButtonElement;
+  if (loginButton) {
+    const loginButtonElement = document.querySelector(
+      "button[name='loginButton']"
+    ) as HTMLButtonElement;
 
-  loginButtonElement.addEventListener("click", async () => {
-    console.log("tryna call MESSAGE_ACTIONS.login");
-    await chrome.runtime.sendMessage({ action: MESSAGE_ACTIONS.login });
-  });
-} else {
-  const writeToPrButtonElement = document.querySelector(
-    "button[name='writeToPrButton']"
-  ) as HTMLButtonElement;
+    loginButtonElement.addEventListener("click", async () => {
+      await chrome.runtime.sendMessage({ action: MESSAGE_ACTIONS.login });
+    });
+  } else {
+    const writeToPrButtonElement = document.querySelector(
+      "button[name='writeToPrButton']"
+    ) as HTMLButtonElement;
 
-  writeToPrButtonElement.addEventListener("click", async () => {
-    console.log("tryna call getSong");
-    await chrome.runtime.sendMessage({ action: MESSAGE_ACTIONS.getSong });
-  });
-}
+    writeToPrButtonElement.addEventListener("click", async () => {
+      await chrome.runtime.sendMessage({ action: MESSAGE_ACTIONS.getSong });
+    });
+  }
+};
+
+main();
